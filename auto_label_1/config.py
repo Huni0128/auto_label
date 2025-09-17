e8bf542eb3c19d532893bbd00fd8e30335633848
Huni0128 <tlacogns@gmail.com>
2025-09-17 10:10:23 +0900

    del: 기존 auto_label 삭제

--- auto_label_1/config.py
+++
@@ -1,8 +0,0 @@
-from typing import Tuple
-
-# ===================== 사용자 설정 =====================
-TARGET_SIZE: Tuple[int, int] = (1280, 720)
-VALID_EXTS = (".png", ".jpg", ".jpeg", ".bmp", ".gif", ".tif", ".tiff", ".webp")
-MAX_THREADS_CAP = 32
-LOG_EVERY_N = 100
-SAVE_JPEG_SPEED_PARAMS = dict(quality=85, subsampling=1, optimize=False, progressive=False)
--- auto_label_1/signals.py
+++
@@ -1,6 +0,0 @@
-from PyQt5.QtCore import QObject, pyqtSignal
-
-
-class Signals(QObject):
-    one_done = pyqtSignal(bool, str)  # (success, message)
-    all_done = pyqtSignal()           # 전체 배치 종료
